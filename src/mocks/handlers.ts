6a4b9f4dcc35be0f6b19bae1551326f68a301245
jonghun <jonghun@jonghunui-noteubug.local>
2023-01-07 02:16:03 +0900

    feat : ai task id 반환 mock api 구현

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,9 +2,16 @@ import { rest } from 'msw';
 
 const dummy = '테스트입니다.';
 
+const dummyTaskId = { task_id: '1' };
+
 export const handlers = [
   // 테스트 mock api
   rest.get('api/v1/test', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(dummy));
+  }),
+
+  // ai task id 반환 api
+  rest.post('api/v1/tasks', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(dummyTaskId));
   }),
 ];
