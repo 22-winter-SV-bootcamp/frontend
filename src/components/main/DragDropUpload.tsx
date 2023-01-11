f09f245bf327a2fc5c9a280c839a505ce6b57065
jonghun <jonghun@jonghunui-noteubug.local>
2023-01-11 20:24:42 +0900

    feat : 업로드 컴포넌트 ui 구현ㄴ

--- src/components/main/DragDropUpload.tsx
+++ src/components/main/DragDropUpload.tsx
@@ -1,8 +1,12 @@
 import React, { DragEvent, useState } from 'react';
 import UploadPng from '/Upload.png';
+import bart from '../../assets/bart.png';
+import titleone from '../../assets/titleone.png';
+import titletwo from '../../assets/titletwo.png';
 import { postTaskId } from '@/apis/postTaskId';
 
-import { Box } from '@mui/material';
+import { Box, Hidden } from '@mui/material';
+import { display, height, padding, positions } from '@mui/system';
 
 const DragDropUpload = () => {
   const UploadFileStyle = {};
@@ -23,13 +27,94 @@ const DragDropUpload = () => {
     })();
   };
   return (
-    <>
-      <Box onDragOver={handleDragOver} onDrop={handleDrop}>
-        <Box>
-          <Box component="img" src={UploadPng} alt="Upload" />
+    <Box
+      sx={{
+        backgroundColor: '#F8CD24',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          width: '100%',
+          minWidth: 400,
+          maxWidth: 600,
+          height: '100%',
+          position: 'relative',
+          overflow: 'hidden',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            width: '100%',
+            height: '25%',
+          }}
+        >
+          <Box
+            component="img"
+            src={titleone}
+            alt="titleone"
+            sx={{ width: 250, marginRight: 13 }}
+          />
+          <Box
+            component="img"
+            src={titletwo}
+            alt="titletwo"
+            sx={{ width: 200, marginLeft: 18 }}
+          />
         </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '55%',
+            marginTop: -6,
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '50%',
+              height: '70%',
+
+              borderRadius: '10%',
+              backgroundColor: 'rgba(255,255,255,0.7)',
+              border: '3px dashed black',
+            }}
+          >
+            <Box
+              component="img"
+              src={UploadPng}
+              alt="UploadIcon"
+              onDragOver={handleDragOver}
+              onDrop={handleDrop}
+            />
+          </Box>
+        </Box>
+
+        <Box
+          component="img"
+          src={bart}
+          sx={{
+            right: 0,
+            bottom: 0,
+
+            position: 'absolute',
+            maxWidth: 300,
+            maxHeight: 300,
+          }}
+        ></Box>
       </Box>
-    </>
+    </Box>
   );
 };
 
