a1a819d1e1851b8612228c363b965ae7e3d115e7
정대영 <eodud1025@naver.com>
2023-01-12 20:19:36 +0900

    feat: 13branch first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
-
-import { EasterEgg } from './components/user/EasterEgg';
 
 import router from './router/router';
 
@@ -12,7 +8,6 @@ function App() {
   return (
     <>
       <RouterProvider router={router}></RouterProvider>
-      <EasterEgg />
     </>
   );
 }
--- src/pages/Custom.tsx
+++ src/pages/Custom.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import { EasterEgg } from '@/components/user/EasterEgg';
+import React from 'react';
 
 const Custom = () => {
-  return <div>Custom Page</div>;
+  return (
+    <>
+      <div>Custom Page</div>
+      <EasterEgg />
+    </>
+  );
 };
 
 export default Custom;
