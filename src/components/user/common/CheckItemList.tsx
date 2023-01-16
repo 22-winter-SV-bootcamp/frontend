1102db1b54ae34e41839677055b6d50601b2514a
jonghun <minn4072@gmail.com>
2023-01-17 05:48:16 +0900

    feat: 커스텀 페이지 버튼 컴포넌트
    # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지
    # 예) feat : 로그인 기능 추가
    
    # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    ################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    ################~

--- src/components/user/common/CheckItemList.tsx
+++ src/components/user/common/CheckItemList.tsx
@@ -1,13 +1,21 @@
+import { Button } from '@mui/material';
 import { Box, Stack } from '@mui/system';
 import React from 'react';
 
 const CheckItemList = (props: any) => {
   return (
     <Box>
-      <Box>Style</Box>
-      <Stack>
-        {[1, 2, 3].map((v) => (
-          <Box>이미지 {v}</Box>
+      <Box>{props.title}</Box>
+      <Stack spacing={1}>
+        {props.arr.map((v: any) => (
+          <Button
+            key={v}
+            onClick={() => {
+              props.setInfo((pre: any) => ({ ...pre, [props.select]: v }));
+            }}
+          >
+            {v}
+          </Button>
         ))}
       </Stack>
     </Box>
