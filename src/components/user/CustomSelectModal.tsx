6073b92d28f2ba23bf12f82c290850cdb2f84151
jonghun <minn4072@gmail.com>
2023-01-17 05:49:15 +0900

    feat: 커스텀 모달 구현
    # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지
    # 예) feat : 로그인 기능 추가
    
    # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    ################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    ################~

--- src/components/user/CustomSelectModal.tsx
+++ src/components/user/CustomSelectModal.tsx
@@ -1,13 +1,92 @@
+import { Button } from '@mui/material';
 import { Box } from '@mui/system';
-import React from 'react';
+import React, { SetStateAction } from 'react';
 import CheckItemList from './common/CheckItemList';
 
-const CustomSelectModal = () => {
+const CustomSelectModal = ({
+  setModal,
+  select,
+  setInfo,
+}: {
+  setModal: React.Dispatch<SetStateAction<boolean>>;
+  select: string;
+  setInfo: any;
+}) => {
+  const colorArr = {
+    white: '#F9F6F6',
+    cream: '#292121',
+    black: '#32454C',
+    gary: '#B8BFDA',
+    pink: '#E4007F',
+    red: '#FF0000',
+    orange: '#FE8B10',
+    yellow: '#FFF174',
+    green: '#60906D',
+    blue: '#0809FF',
+    navy: '#3E4982',
+    purple: '#E179E3',
+    brown: '#886024',
+    beige: '#CEB37D',
+  };
+  const styles: any = {
+    hair: {
+      short: 'a',
+      long: 'b',
+      middle: 'c',
+    },
+    top: {
+      // TODO: -blazer 원래는 이거임
+      blazer: '@/assets/custom/bart.png',
+      blouse: '@/assets/custom/bart.png',
+      cardigan: '@/assets/custom/bart.png',
+      coat: '@/assets/custom/bart.png',
+      denim: '@/assets/custom/bart.png',
+      hoodi: '@/assets/custom/bart.png',
+      jumper: '@/assets/custom/bart.png',
+      mustang: '@/assets/custom/bart.png',
+      riders: '@/assets/custom/bart.png',
+      shirts: '@/assets/custom/bart.png',
+      pkshirts: '@/assets/custom/bart.png',
+
+      sweatshirts: '@/assets/custom/bart.png',
+    },
+    bottom: {
+      leggings: '@/assets/custom/bart.png',
+      sweatpants: '@/assets/custom/bart.png',
+      slacks: '@/assets/custom/bart.png',
+      skirt: '@/assets/custom/bart.png',
+      sweater: '@/assets/custom/bart.png',
+      // TODO: Half-shortpants 원래는 이거임
+      HalfShortpants: '@/assets/custom/bart.png',
+    },
+    background: {
+      type1: 'd',
+      type2: 'e',
+      type3: 'f',
+    },
+  };
+
   return (
     <Box>
-      <CheckItemList></CheckItemList>
-      <CheckItemList></CheckItemList>
-      <Box>Done</Box>
+      <CheckItemList
+        select={select}
+        setInfo={setInfo}
+        title="Style"
+        arr={Object.keys(styles[select])}
+      ></CheckItemList>
+      <CheckItemList
+        select={select + 'Color'}
+        setInfo={setInfo}
+        title="Color"
+        arr={Object.keys(colorArr)}
+      ></CheckItemList>
+      <Button
+        onClick={() => {
+          setModal(false);
+        }}
+      >
+        <Box>OK</Box>
+      </Button>
     </Box>
   );
 };
