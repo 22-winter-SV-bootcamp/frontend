d5e4345272ef3827cd046aeb97e58f19a6b074ec
jonghun <minn4072@gmail.com>
2023-01-17 05:46:17 +0900

    feat: 심슨 공용 컴포넌트 추가
    # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지
    # 예) feat : 로그인 기능 추가
    
    # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    ################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    ################~

--- src/components/user/common/CheckItem.tsx
+++ src/components/user/common/CheckItem.tsx
@@ -1,13 +1,26 @@
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 
 import React from 'react';
 
 const CheckItem = (props: any) => {
+  let { setModal } = props;
+
   return (
     <Box>
       <Typography>{props.title}</Typography>
-      <Box component="img" src={''} alt={props.info[props.title]}></Box>
+      <Button
+        onClick={() => {
+          setModal(true);
+          props.setSelect(props.title);
+        }}
+      >
+        <Box
+          component="img"
+          src={`@/assets/custom/${props.title}/${props.info[props.title]}.png`}
+          alt={props.info[props.title]}
+        ></Box>
+      </Button>
     </Box>
   );
 };
