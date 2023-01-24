4cf07a89eba6ac37f998b3594c143d54cd90fac8
jonghun <minn4072@gmail.com>
2023-01-24 13:26:30 +0900

    docs : svgComponents female add

--- src/svgComponents/female/top/SVGFemaleBlouse.tsx
+++ src/svgComponents/female/top/SVGFemaleBlouse.tsx
@@ -4,11 +4,9 @@ const SVGFemaleBlouse = ({ main, sub, detail }: any) => (
     id="\uBE14\uB77C\uC6B0\uC2A4"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 595.2 595.2"
-    width="100%"
-    height="100%"
   >
     <defs>
-      <style>{`.cls-1{fill:${main}}`}</style>
+      <style>{'.cls-1{fill:#fff;}'}</style>
     </defs>
     <g id="\uB808\uC774\uC5B4_50" data-name="\uB808\uC774\uC5B4 50">
       <path
