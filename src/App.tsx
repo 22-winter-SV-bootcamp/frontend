b82f5aa51ade8b56fe6d267cffae8b74c0777bc4
jonghun <minn4072@gmail.com>
2023-02-03 05:31:41 +0900

    refact : 이미지 추가 및 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
 import { RouterProvider } from 'react-router-dom';
-import { EasterEgg } from './components/user/EasterEgg';
-import Simpson from './assets/Simpson.png';
-import Simpson2 from './assets/Simpson2.png';
-import Simpson3 from './assets/Simpson3.png';
-import SimpsonDoh from './assets/SimpsonDoh.jpg';
-import SimpsonDoh2 from './assets/SimpsonDoh2.png';
 
 import router from './router/router';
-import { ResultPage } from './components/result/ResultPage';
-import { UploadPage } from './components/upload/UploadPage';
-import Test1 from './svgComponents/female/hair/SVGFemaleLong';
-import Test2 from './svgComponents/female/inner/SVGFemaleBasic_neck_pole';
-import Test3 from './svgComponents/female/bottom/SVGFemaleDenim';
-import Test4 from './svgComponents/female/top/SVGFemaleBlazer2';
 
 function App() {
   return <RouterProvider router={router}></RouterProvider>;
-  // return (
-  //   <>
-  //     <Test2 viewBox="-42 114 550 550" />
-  //     <Test3 />
-  //     <Test4 />
-  //     <Test1 main="blue" sub="50" />
-  //   </>
-  // );
 }
 export default App;
--- src/apis/createAxios.ts
+++ src/apis/createAxios.ts
@@ -2,7 +2,8 @@ import { ImportExport } from '@mui/icons-material';
 import axios from 'axios';
 
 const axiosCustom = axios.create({
-  baseURL: 'https://api.simpsonfilm.com',
+  // baseURL: 'https://api.simpsonfilm.com',
+  baseURL: 'http://localhost',
 });
 
 export default axiosCustom;
