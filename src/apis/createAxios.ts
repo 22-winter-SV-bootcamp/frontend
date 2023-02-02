fb088055f9f8ad0482508b9dfdd2b98769e0d4f1
Ellie <doris0707@naver.com>
2023-02-02 20:46:31 +0900

    chore: deploy set

--- src/apis/createAxios.ts
+++ src/apis/createAxios.ts
@@ -2,7 +2,7 @@ import { ImportExport } from '@mui/icons-material';
 import axios from 'axios';
 
 const axiosCustom = axios.create({
-  baseURL: 'http://localhost',
+  baseURL: 'https://api.simpsonfilm.com',
 });
 
 export default axiosCustom;
