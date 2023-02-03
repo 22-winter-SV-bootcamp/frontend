650e9276cb4a14f6952655b7fdf27a0cfa31c163
jonghun <minn4072@gmail.com>
2023-02-03 21:45:09 +0900

    refact: axios baseurl 수정정

--- src/apis/createAxios.ts
+++ src/apis/createAxios.ts
@@ -2,7 +2,7 @@ import { ImportExport } from '@mui/icons-material';
 import axios from 'axios';
 
 const axiosCustom = axios.create({
-  baseURL: !import.meta.env.DEV
+  baseURL: import.meta.env.DEV
     ? 'http://localhost'
     : 'https://api.simpsonfilm.com',
 });
